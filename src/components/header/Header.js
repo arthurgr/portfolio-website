ea2a84a974f40426b2d5cbf17d645137bd5b1a40
Arthur Revelski <arthurrevelski@Arthur-Revelskis-iMac.local>
2021-01-02 16:24:49 -0700

    Cleaned up scss

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,34 +10,32 @@ const Header = () => {
     const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }));
     
     return (
-        <div className={classNames(["color-background","container"])} id="side" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+        <header onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
             <div id="top">
                 <div className={classNames(["inside"])}>
-                    <div id="profile-pic">
-                        <img id="profile-pic-img" src={img} alt="Photo of Arthur Revelski" />
-                    </div>
+                    <img id="profile-pic-img" src={img} alt="Photo of Arthur Revelski" />
                     <h1 id="name">Arthur Revelski</h1>
                     <h2 id="current-position"><b>UI/UX Developer - React JS</b></h2>
                     <h2 id="current-location"><b>Lakewood, CO</b></h2>
-                    <p id="about">I’m passionate about designing and developing intuitive user interfaces. I enjoy trying out new recipes, exploring the Colorado mountains, and staying active.</p>
-                    <ul className={classNames(["subContact"])}>
-                    <li>
-                        <a href="mailto:arevelski@gmail.com" title="Email Arthur" ><b>Email Arthur</b></a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/arthurgr" title="Code Examples" target="_blank"><b>Github</b></a>
-                    </li>
-                    <li>
-                        <a href="https://dribbble.com/arthurrevelski" title="Design Work" target="_blank"><b>Dribble</b></a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/arthur-revelski-75894054/" title="Professional Experience" target="_blank"><b>LinkedIn</b></a>
-                    </li>
+                    <p>I’m passionate about designing and developing intuitive user interfaces. I enjoy trying out new recipes, exploring the Colorado mountains, and staying active.</p>
+                    <ul>
+                        <li>
+                            <a href="mailto:arevelski@gmail.com" title="Email Arthur" ><b>Email Arthur</b></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/arthurgr" title="Code Examples" target="_blank"><b>Github</b></a>
+                        </li>
+                        <li>
+                            <a href="https://dribbble.com/arthurrevelski" title="Design Work" target="_blank"><b>Dribble</b></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/arthur-revelski-75894054/" title="Professional Experience" target="_blank"><b>LinkedIn</b></a>
+                        </li>
                     </ul>
                 </div>
             </div>
-            <animated.div className={classNames(["card1"])} style={{ transform: props.xy.interpolate(trans1) }}></animated.div>
-        </div>
+            <animated.div className={classNames(["animated-bg"])} style={{ transform: props.xy.interpolate(trans1) }}></animated.div>
+        </header>
     );
 }
 
